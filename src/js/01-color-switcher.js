7567d4161844aee8121ec5bacce8a915545ac10f
Zyabrik10 <sasamazurok@gmail.com>
2023-04-23 00:09:29 +0300

    1 б 3 вщту

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const start_button = document.querySelector('button[data-start]');
+const stop_button = document.querySelector('button[data-stop]');
 
+let timer;
+
+start_button.addEventListener('click', () => {
+    timer = setInterval(() => {
+       document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    start_button.disabled = true;
+});
+
+stop_button.addEventListener('click', () => {
+    clearInterval(timer);
+
+    start_button.disabled = false;
+});
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    return Promise.resolve({position, delay});
   } else {
-    // Reject
+    return Promise.reject({position, delay});
   }
 }
+
+document.querySelector('.form').addEventListener('submit', event =>{
+  event.preventDefault();
+
+  const delay = Number(document.querySelector('input[name=delay]').value);
+  const step = Number(document.querySelector('input[name=step]').value);
+  const amount = Number(document.querySelector('input[name=amount]').value);
+
+  setTimeout(()=>{
+    let counter = 1;
+
+    createPromise(counter, delay).then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+    let timer = setInterval(()=>{
+      if (counter >= amount){
+        clearInterval(timer);
+        return;
+      }
+      
+      counter++;
+      createPromise(counter, delay + step * (counter - 1)).then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    }, step);
+  }, delay);
+});
